6e3e8fc3b50d220ea4010336473c64c3afc951a5
ARDhruvo <dhruvo.rahman0705@gmail.com>
2024-10-24 11:01:34 +0600

    Finalized with extra comments

--- Balanced Parantheses.cpp
+++ Balanced Parantheses.cpp
@@ -1,21 +1,22 @@
-/* @Author: ARDhruvo/Kashikizu
+/*
+ * @Author: ARDhruvo/Kashikizu
  * Initial Completion Date: 24/10/2024 (DD/MM/YYYY)
  */
 
 /* The main strategy is:
  * 1. If the character is an opening paranthesis then we push it in the stack
- * 
- * 
+ *
+ *
  * 2. IF the character is a closing paranthesis and:
- * 
+ *
  * 2.1. -> If the stack is empty then its not balanced <-
- * 
+ *
  * 2.2. If the stack is not empty:
  * 2.2.1. -> If the top of the stack is not the same type of closing paranthesis then its not balanced <-
  * 2.2.2. If the top of the stack is the same type of closing paranthesis then we pop
- * 
- * 
- * 3. If the stack is empty at the end of the loop then it is balanced 
+ *
+ *
+ * 3. If the stack is empty at the end of the loop then it is balanced
  */
 
 #include <bits/stdc++.h>
@@ -26,13 +27,24 @@ bool balanced(string inp)
     stack<char> parantheses;
     for (int i = 0; i < inp.size(); i++)
     {
-        if (inp[i] == '(') // Pushes in stack if parantheses is open
+        if ((inp[i] == '(') || (inp[i] == '{') || (inp[i] == '[')) // Case 1
         {
             parantheses.push(inp[i]);
         }
-        else if (inp[i] == ')') // Pops if parantheses is closed
+        else if ((inp[i] == ')') || (inp[i] == '}') || (inp[i] == ']'))
         {
-            parantheses.pop();
+            if (parantheses.empty())
+            {
+                return false; // Case 2.1
+            }
+            else if (((inp[i] == ')') && (parantheses.top() != '(')) || ((inp[i] == '}') && (parantheses.top() != '{')) || ((inp[i] == ']') && (parantheses.top() != '[')))
+            {
+                return false; // Case 2.2
+            }
+            else
+            {
+                parantheses.pop(); // Pops if parantheses is closed
+            }
         }
     }
     if (parantheses.empty()) // Returns true or false for future scalability. Otherwise would have been printed here
@@ -50,7 +62,6 @@ int main()
     string inp, exp;
     cout << "Enter expression: ";
     getline(cin, inp); // cin fails to take white spaces for some reason
-    cout << inp << endl;
     if (balanced(inp))
     {
         cout << "Balanced" << endl;
